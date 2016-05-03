b5db0b91f596d72f8733e53cddc575ed99aa1629
Isaac <isaacfernandezmoro@gmail.com>
2016-05-03 11:59:42 +0100

    Heuristica casi terminada, hay que mejorar los valores de si encuentra un punto o un circulo o una equis

--- Week 4 - Conecta 4/heuristics.py
+++ Week 4 - Conecta 4/heuristics.py
@@ -8,66 +8,58 @@ def random_heuristic(state=None):
     return random.randint(-100, 100)
 
 
-def best_direction(state=None):
-    posibilidades = [0, 0, 0]
-
-    posibilidades[0] = posibles_verticales()
-    posibilidades[1] = posibles_horizontales()
-    posibilidades[2] = posibles_diagonales()
-
-    return devuelve_maximo(posibilidades)
-
-
-def devuelve_maximo(lista):
-    lista.sort()
-    return lista[lista.__len__() - 1]
-
-
-def posibles_verticales(state=None):
-    acumulated_value = 0;
-
-    columna = 1
-    for fila in range(1, 7):
-        if state.board.get(columna, fila) == ".":
-            acumulated_value += 100;
-        if state.board.get(columna, fila) == game.to_move(state):
-            acumulated_value += 1000;
-        if state.board.get(columna, fila) != game.to_move(state):
-            acumulated_value -= 100;
-
-        columna += 1
-        return acumulated_value
-
-
-def posibles_horizontales(state=None):
-    acumulated_value = 0;
-
-    fila = 1
+def foo_heuristic(state):
+    result = 0
     for columna in range(1, 7):
-        if state.board.get(columna, fila) == ".":
-            acumulated_value += 100;
-        if state.board.get(columna, fila) == game.to_move(state):
-            acumulated_value += 1000;
-        if state.board.get(columna, fila) != game.to_move(state):
-            acumulated_value -= 100;
-
-        fila += 1
-        return acumulated_value
+        for fila in range(1, 8):
+            if (columna, fila) in state.moves:
+                result += best_direction(tupla=(columna,fila), state=state)
+                break
+    return result
 
 
+def best_direction(tupla=None, state=None):
+    return posibles_horizontales(tupla, state) + posibles_verticales(tupla, state)
+
+def posibles_verticales(tupla, state=None):
+    acumulated_value = 0
+
+    for fila in range(tupla[1], 7):
+        if (tupla[0], fila) in state.moves:
+            acumulated_value += 100
+
+        if state.board.get((tupla[0], fila)) == game.to_move(state):
+            acumulated_value += 1000
+        if state.board.get((tupla[0], fila)) != game.to_move(state):
+            acumulated_value -= 10000
+    return acumulated_value
+
+
+def posibles_horizontales(tupla, state=None):
+    acumulated_value = 0
+    num = tupla[0]
+    for columna in range(tupla[0], 7):
+        if (columna, tupla[1]) in state.moves:
+            acumulated_value += 100
+        if state.board.get((columna, tupla[1])) == game.to_move(state):
+            acumulated_value += 1000
+        if state.board.get((columna, tupla[1])) != game.to_move(state):
+            acumulated_value -= 10000
+
+    return acumulated_value
+
+"""
 def posibles_diagonales(state=None):
-    acumulated_value = 0;
+    acumulated_value = 0
 
-    fila = 1
     for columna in range(1, 7):
 
-        if state.board.get(columna, fila) == ".":
-            acumulated_value += 100;
-        if state.board.get(columna, fila) == game.to_move(state):
-            acumulated_value += 1000;
-        if state.board.get(columna, fila) != game.to_move(state):
-            acumulated_value -= 100;
+        if state.board.get((columna, fila)) == ".":
+            acumulated_value += 100
+        if state.board.get((columna, fila)) == game.to_move(state):
+            acumulated_value += 1000
+        if state.board.get((columna, fila)) != game.to_move(state):
+            acumulated_value -= 10000
 
-        columna += 1
-        fila += 1
-        return acumulated_value
+    return acumulated_value
+"""
