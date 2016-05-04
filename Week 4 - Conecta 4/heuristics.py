5e13c552bb3b9f28fb7d0d3388677fc389c57870
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-05-04 21:02:27 +0100

    Refactor: rename

--- Week 4 - Conecta 4/heuristics.py
+++ Week 4 - Conecta 4/heuristics.py
@@ -19,13 +19,16 @@ def best_move_heuristic(state=None):
     for column in range(1, 7):
         for row in range(1, 8):
             if (column, row) in state.moves:
-                result += best_direction(tuple=(column, row), state=state)
+                result += best_direction(coordinate=(column, row), state=state)
                 break
     return result
 
 
-def best_direction(tuple, state):
-    return horizontal(tuple, state) + vertical(tuple, state) + diagonal(tuple, state) + inverse_diagonal(tuple, state)
+def best_direction(coordinate, state):
+    return horizontal(coordinate, state) \
+           + vertical(coordinate, state) \
+           + diagonal(coordinate, state) \
+           + inverse_diagonal(coordinate, state)
 
 
 values = {
@@ -36,26 +39,26 @@ values = {
 }
 
 
-def vertical(tuple, state):
+def vertical(coordinate, state):
     index = 1
     top_in_row = 0
     bottom_in_row = 0
     total_value = 0
 
-    bottom_player = state.board.get((tuple[0], tuple[1] - 1))
+    bottom_player = state.board.get((coordinate[0], coordinate[1] - 1))
     if bottom_player is not None:
         while index < 4:
-            if state.board.get((tuple[0], tuple[1] - index)) == bottom_player:
+            if state.board.get((coordinate[0], coordinate[1] - index)) == bottom_player:
                 bottom_in_row += 1
             else:
                 break
             index += 1
 
     index = 1
-    top_player = state.board.get((tuple[0], tuple[1] + 1))
+    top_player = state.board.get((coordinate[0], coordinate[1] + 1))
     if top_player is not None:
         while index < 4:
-            if state.board.get((tuple[0], tuple[1] + index)) == top_player:
+            if state.board.get((coordinate[0], coordinate[1] + index)) == top_player:
                 top_in_row += 1
             else:
                 break
@@ -73,26 +76,26 @@ def vertical(tuple, state):
     return total_value
 
 
-def horizontal(tuple, state):
+def horizontal(coordinate, state):
     index = 1
     left_in_row = 0
     right_in_row = 0
     total_value = 0
 
-    left_player = state.board.get((tuple[0] - 1, tuple[1]))
+    left_player = state.board.get((coordinate[0] - 1, coordinate[1]))
     if left_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] - index, tuple[1])) == left_player:
+            if state.board.get((coordinate[0] - index, coordinate[1])) == left_player:
                 left_in_row += 1
             else:
                 break
             index += 1
 
     index = 1
-    right_player = state.board.get((tuple[0] + 1, tuple[1]))
+    right_player = state.board.get((coordinate[0] + 1, coordinate[1]))
     if right_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] + index, tuple[1])) == right_player:
+            if state.board.get((coordinate[0] + index, coordinate[1])) == right_player:
                 right_in_row += 1
             else:
                 break
@@ -111,26 +114,26 @@ def horizontal(tuple, state):
     return total_value
 
 
-def diagonal(tuple, state):
+def diagonal(coordinate, state):
     index = 1
     top_right_in_row = 0
     left_bottom_in_row = 0
     total_value = 0
 
-    top_right_player = state.board.get((tuple[0] + 1, tuple[1] + 1))
+    top_right_player = state.board.get((coordinate[0] + 1, coordinate[1] + 1))
     if top_right_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] + index, tuple[1] + index)) == top_right_player:
+            if state.board.get((coordinate[0] + index, coordinate[1] + index)) == top_right_player:
                 top_right_in_row += 1
             else:
                 break
             index += 1
 
     index = 1
-    left_bottom_player = state.board.get((tuple[0] - 1, tuple[1] - 1))
+    left_bottom_player = state.board.get((coordinate[0] - 1, coordinate[1] - 1))
     if left_bottom_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] + index, tuple[1])) == left_bottom_player:
+            if state.board.get((coordinate[0] + index, coordinate[1])) == left_bottom_player:
                 left_bottom_in_row += 1
             else:
                 break
@@ -149,26 +152,26 @@ def diagonal(tuple, state):
     return total_value
 
 
-def inverse_diagonal(tuple, state):
+def inverse_diagonal(coordinate, state):
     index = 1
     top_left_in_row = 0
     right_bottom_in_row = 0
     total_value = 0
 
-    top_left_player = state.board.get((tuple[0] - 1, tuple[1] + 1))
+    top_left_player = state.board.get((coordinate[0] - 1, coordinate[1] + 1))
     if top_left_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] - index, tuple[1] + index)) == top_left_player:
+            if state.board.get((coordinate[0] - index, coordinate[1] + index)) == top_left_player:
                 top_left_in_row += 1
             else:
                 break
             index += 1
 
     index = 1
-    right_bottom_player = state.board.get((tuple[0] + 1, tuple[1] - 1))
+    right_bottom_player = state.board.get((coordinate[0] + 1, coordinate[1] - 1))
     if right_bottom_player is not None:
         while index < 4:
-            if state.board.get((tuple[0] + index, tuple[1] - index)) == right_bottom_player:
+            if state.board.get((coordinate[0] + index, coordinate[1] - index)) == right_bottom_player:
                 right_bottom_in_row += 1
             else:
                 break
