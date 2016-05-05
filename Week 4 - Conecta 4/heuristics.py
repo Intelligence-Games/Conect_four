540225bb0092e5d875c973a548fdcde4d7498ab8
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-05-05 01:00:31 +0100

    Reset master to work realized in laboratory

--- Week 4 - Conecta 4/heuristics.py
+++ Week 4 - Conecta 4/heuristics.py
@@ -1,40 +1,153 @@
 import random
 
+from connectfour import ConnectFour
+
+game = ConnectFour()
+
 
 def random_heuristic(state=None):
     return random.randint(-100, 100)
 
 
+def best_move_heuristic(state=None):
+    result = 0
+
+    if state.utility == 1:
+        return state.utility * 10000000
+    if state.utility == -1:
+        return state.utility * 5000000
+
+    for column in range(1, 7):
+        for row in range(1, 8):
+            if (column, row) in state.moves:
+                result += best_direction(coordinate=(column, row), state=state)
+                break
+    return result
 
 
-def check_horizontal(state=None):
-    fila = 1
-    columna = 1
-    posicion = (fila, columna)
-
-    for fila in state.board(fila,columna):
-        huecos_izq = check_left_consecutive_hole(columna,state.moves)
-        rig = check_right_consecutive_hole(columna,state.moves)
-
-def check_left_consecutive_hole(columna,moves):
-    fila = 1
-    numero_de_huecos = 0
-    for fila in moves(fila,columna):
-        numero_de_huecos + 1
-        fila+1
-
-    print numero_de_huecos;
-
-def check_right_consecutive_hole(player,columna):
-    pass
+def best_direction(coordinate, state):
+    return horizontal(coordinate, state) \
+           + vertical(coordinate, state) \
+           + diagonal(coordinate, state) \
+           + inverse_diagonal(coordinate, state)
 
 
+values = {
+    0: 0,
+    1: 100,
+    2: 1000,
+    3: 10000
+}
 
-#       Instrucciones !!!
-# Tenemos que buscar en la misma columa, hacia arriba cuantas posiciones tienen resultado de "state.board.get(columa,fila)"
-# igual a "." Si se encuentra con una ficha del jugador contrario al hacer el "state.board.get(columa,fila)" entonces disminuye
-# el valor de esa direccion y si encuentra una hilera de 4 espacios vacios tiene mayor valor. Son mejores las hileras en horizontal,
-# despues en vertical y por último las diagonales, porque para hacer un 4 en raya en diagonal necesitas poner muchas más piezas
-#si un poco, al principio recorriendo las 7 posibilidades osea la fila 1, recorrer las columnas y preguntar con state.board
-#(fila, columna) la posibilidad de cuatro en raya que tiene, y de las 7 posibilidades que tienes de colocar la ficha devolver
-#la de mayor valor
+
+def calculate_in_row(player, board, coordinate):
+    in_row = 0
+    if player is not None:
+        index = 1
+        while index < 4:
+            if board.get((coordinate[0] - index, coordinate[1])) == player:
+                in_row += 1
+            else:
+                break
+            index += 1
+    return in_row
+
+
+def vertical(coordinate, state):
+    board = state.board
+    coordinate_x = coordinate[0]
+    coordinate_y = coordinate[1]
+
+    bottom_player = state.board.get((coordinate_x, coordinate_y - 1))
+    top_in_row = calculate_in_row(player=bottom_player, board=board, coordinate=coordinate)
+
+    top_player = state.board.get((coordinate_x, coordinate_y + 1))
+    bottom_in_row = calculate_in_row(player=top_player, board=board, coordinate=coordinate)
+
+    total_value = 0
+    if bottom_player == game.to_move(state):
+        total_value += values[bottom_in_row]
+    else:
+        total_value -= values[bottom_in_row]
+
+    if top_player == game.to_move(state):
+        total_value += values[top_in_row]
+    else:
+        total_value -= values[top_in_row]
+
+    return total_value
+
+
+def horizontal(coordinate, state):
+    board = state.board
+    coordinate_x = coordinate[0]
+    coordinate_y = coordinate[1]
+
+    left_player = board.get((coordinate_x - 1, coordinate_y))
+    left_in_row = calculate_in_row(player=left_player, board=board, coordinate=coordinate)
+
+    right_player = board.get((coordinate_x + 1, coordinate_y))
+    right_in_row = calculate_in_row(player=right_player, board=board, coordinate=coordinate)
+
+    total_value = 0
+    if left_player == game.to_move(state):
+        total_value += values[left_in_row]
+    else:
+        total_value -= values[left_in_row]
+
+    if right_player == game.to_move(state):
+        total_value += values[right_in_row]
+    else:
+        total_value -= values[right_in_row]
+
+    return total_value
+
+
+def diagonal(coordinate, state):
+    board = state.board
+    coordinate_x = coordinate[0]
+    coordinate_y = coordinate[1]
+
+    top_right_player = state.board.get((coordinate_x + 1, coordinate_y + 1))
+    top_right_in_row = calculate_in_row(player=top_right_player, board=board, coordinate=coordinate)
+
+    left_bottom_player = state.board.get((coordinate_x - 1, coordinate_y - 1))
+    left_bottom_in_row = calculate_in_row(player=left_bottom_player, board=board, coordinate=coordinate)
+
+    total_value = 0
+    if left_bottom_player == game.to_move(state):
+        total_value += values[left_bottom_in_row]
+    else:
+        total_value -= values[left_bottom_in_row]
+
+    if top_right_player == game.to_move(state):
+        total_value += values[top_right_in_row]
+    else:
+        total_value -= values[top_right_in_row]
+
+    return total_value
+
+
+def inverse_diagonal(coordinate, state):
+    board = state.board
+    coordinate_x = coordinate[0]
+    coordinate_y = coordinate[1]
+
+    top_left_player = state.board.get((coordinate_x - 1, coordinate_y + 1))
+    top_left_in_row = calculate_in_row(player=top_left_player, board=board, coordinate=coordinate)
+
+    right_bottom_player = state.board.get((coordinate_x + 1, coordinate_y - 1))
+    right_bottom_in_row = calculate_in_row(player=right_bottom_player, board=board, coordinate=coordinate)
+
+    total_value = 0
+    if top_left_player == game.to_move(state):
+        total_value += values[top_left_in_row]
+    else:
+        total_value -= values[top_left_in_row]
+
+    if right_bottom_player == game.to_move(state):
+        total_value += values[right_bottom_in_row]
+    else:
+        total_value -= values[right_bottom_in_row]
+
+    return total_value
